ba6c5692df49af2909e0ff8243bfe8d6c57eca10
styughjvbn <3338761@naver.com>
2023-05-30 18:21:31 +0900

    <fix> : run.py 오류 수정 -------------------------------- 1. 함수 변경

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ from bleu import _bleu
 from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampler,TensorDataset
 from torch.utils.data.distributed import DistributedSampler
 from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
-                          RobertaConfig, RobertaModel, RobertaTokenizer,AutoModelForMaskedLM,AutoTokenizer)
+                          RobertaConfig, RobertaModel, RobertaTokenizer,AutoModel,AutoTokenizer,AutoConfig)
 MODEL_CLASSES = {'roberta': (RobertaConfig, RobertaModel, RobertaTokenizer)}
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
@@ -265,31 +265,11 @@ def main():
     if os.path.exists(args.output_dir) is False:
         os.makedirs(args.output_dir)
         
-    config = {
-  "architectures": [
-    "RobertaForMaskedLM"
-  ],
-  "attention_probs_dropout_prob": 0.1,
-  "bos_token_id": 0,
-  "eos_token_id": 2,
-  "hidden_act": "gelu",
-  "hidden_dropout_prob": 0.1,
-  "hidden_size": 768,
-  "initializer_range": 0.02,
-  "intermediate_size": 3072,
-  "layer_norm_eps": 1e-05,
-  "max_position_embeddings": 514,
-  "model_type": "roberta",
-  "num_attention_heads": 12,
-  "num_hidden_layers": 6,
-  "pad_token_id": 1,
-  "type_vocab_size": 1,
-  "vocab_size": 52000
-}
+    config = AutoConfig.from_pretrained("huggingface/CodeBERTa-small-v1")
     tokenizer = AutoTokenizer.from_pretrained("huggingface/CodeBERTa-small-v1")
     
     #budild model
-    encoder = AutoModelForMaskedLM.from_pretrained("huggingface/CodeBERTa-small-v1")
+    encoder = RobertaModel.from_pretrained("huggingface/CodeBERTa-small-v1")
     decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
     decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
     model=Seq2Seq(encoder=encoder,decoder=decoder,config=config,
