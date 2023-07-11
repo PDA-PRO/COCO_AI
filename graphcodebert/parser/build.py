23a99883118a914486bf5d22c58af4c7ddd1ad0b
styughjvbn <3338761@naver.com>
2023-07-11 22:24:51 +0900

    <fix> : tree_sitter 오류 수정 -------------------------------- 1.  tree_sitter 오류 수정

--- graphcodebert/parser/build.py
+++ graphcodebert/parser/build.py
@@ -9,13 +9,7 @@ Language.build_library(
 
   # Include one or more languages
   [
-    'tree-sitter-go',
-    'tree-sitter-javascript',
     'tree-sitter-python',
-    'tree-sitter-php',
-    'tree-sitter-java',
-    'tree-sitter-ruby',
-    'tree-sitter-c-sharp',
   ]
 )
 
