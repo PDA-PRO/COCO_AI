5e617b43f1b22a25a0bf5d343551f444a895cd49
styughjvbn <3338761@naver.com>
2025-09-25 14:30:39 +0900

    feat: 추론 엔드포인트 수정

--- wpc/modal_deploy.py
+++ wpc/modal_deploy.py
@@ -4,9 +4,6 @@ import modal
 docker_image = modal.Image.from_dockerfile("Dockerfile.serverless")
 app = modal.App("coco-ai", image=docker_image)
 
-class CodeReq(BaseModel):
-    code: str
-
 snapshot_key = "v1"
 
 with docker_image.imports():
@@ -24,10 +21,36 @@ class Inference:
         self.wpc = WPC()
         print(f"snapshotting {snapshot_key}")
 
-    @modal.fastapi_endpoint(method="POST")
-    def run(self, p_id:str, code_req:CodeReq):
-        try:
-            fixed_code,generalized_code = self.wpc.process(code_req.code,p_id)
-        except Exception as e:
-            return {"error":0}
-        return {'fixed_code':fixed_code,'bug_code':generalized_code}
+    @modal.asgi_app()
+    def fastapi_app(self):
+        from fastapi import FastAPI, HTTPException
+
+        web_app = FastAPI()
+
+        class CodeReq(BaseModel):
+            code: str
+
+        @web_app.post("/process")
+        def run(p_id:str, code_req:CodeReq):
+            try:
+                fixed_code,generalized_code = self.wpc.process(code_req.code,p_id)
+            except Exception as e:
+                raise HTTPException(500,e)
+            return {'fixed_code':fixed_code,'bug_code':generalized_code}
+        
+        @web_app.get('/hello')
+        def test():
+            """
+            모델일 정상적으로 동작하는지 테스트
+
+            returns
+            - 정상동작시 True, 비정상동작시 False
+            """
+
+            try:
+                self.wpc.process('a,b,c=map(int,input().split())\nsound=0\nfor i in range(c):\n    b-=a\nif a<0:\n    break\nelse:\n    sound+=1\nprint(sound)',"p03105")
+            except Exception as e:
+                print(str(e))
+                return False
+            return True
+        return web_app
